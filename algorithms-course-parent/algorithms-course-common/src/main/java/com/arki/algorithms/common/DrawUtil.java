076fccf4e391e548efe15f0ecce2a744079f6526
Rain <rain@example.com>
2018-01-07 22:53:38 +0800

    Remove the listeners of DrawUtil.Pen that are not useful now.

--- algorithms-course-parent/algorithms-course-common/src/main/java/com/arki/algorithms/common/DrawUtil.java
+++ algorithms-course-parent/algorithms-course-common/src/main/java/com/arki/algorithms/common/DrawUtil.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.DirectColorModel;
 import java.awt.image.WritableRaster;
@@ -72,11 +73,14 @@ public class DrawUtil {
 //        }
 //        drawHistogram(list);
         Pen pen = new Pen();
+        pen.setPenColor(Color.BLACK);
+        pen.setPenRadius(100);
+        pen.point(200, 200);
 
     }
 
 
-    private static final class Pen implements ActionListener, MouseListener, MouseMotionListener, KeyListener {
+    private static final class Pen implements ActionListener {
 
 
         /**
@@ -137,12 +141,7 @@ public class DrawUtil {
             ImageIcon icon = new ImageIcon(bufferedImage);
             JLabel draw = new JLabel(icon);
 
-            draw.addMouseListener(this);
-            draw.addMouseMotionListener(this);
-
-
             frame.setContentPane(draw);
-            frame.addKeyListener(this);    // JLabel cannot get keyboard focus
             frame.setResizable(false);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);            // closes all windows
             frame.setTitle("Pen");
@@ -151,6 +150,11 @@ public class DrawUtil {
             frame.requestFocusInWindow();
             frame.setVisible(true);
         }
+
+        /***************************************************************************
+         *  Basic settings.
+         ***************************************************************************/
+
         public void setCanvas(int canvasWidth, int canvasHeight, Color backgroundColor){
             width=canvasWidth;
             height=canvasHeight;
@@ -162,11 +166,11 @@ public class DrawUtil {
             graphics.setColor(color);
         }
         public void setPenRadius(double radius){
-            penRadius=radius;
+            penRadius = radius;
             BasicStroke basicStroke = new BasicStroke((float)radius, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
             graphics.setStroke(basicStroke);
         }
-        public void show(){
+        private void show(){
             graphics.drawImage(bufferedImage, 0, 0, null);
             frame.repaint();
         }
@@ -181,6 +185,10 @@ public class DrawUtil {
             menu.add(menuItem);
             return menuBar;
         }
+
+        /***************************************************************************
+         *  Save drawing to a file.
+         ***************************************************************************/
         public void save(String fileName){
             if (fileName == null) throw new IllegalArgumentException();
             File file = new File(fileName);
@@ -190,8 +198,7 @@ public class DrawUtil {
             if ("png".equalsIgnoreCase(suffix)) {
                 try {
                     ImageIO.write(bufferedImage, suffix, file);
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -201,26 +208,22 @@ public class DrawUtil {
             else if ("jpg".equalsIgnoreCase(suffix)) {
                 WritableRaster raster = bufferedImage.getRaster();
                 WritableRaster newRaster;
-                newRaster = raster.createWritableChild(0, 0, width, height, 0, 0, new int[] {0, 1, 2});
+                newRaster = raster.createWritableChild(0, 0, width, height, 0, 0, new int[]{0, 1, 2});
                 DirectColorModel cm = (DirectColorModel) bufferedImage.getColorModel();
                 DirectColorModel newCM = new DirectColorModel(cm.getPixelSize(),
                         cm.getRedMask(),
                         cm.getGreenMask(),
                         cm.getBlueMask());
-                BufferedImage rgbBuffer = new BufferedImage(newCM, newRaster, false,  null);
+                BufferedImage rgbBuffer = new BufferedImage(newCM, newRaster, false, null);
                 try {
                     ImageIO.write(rgbBuffer, suffix, file);
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
-
-            else {
+            } else {
                 System.out.println("Invalid image file type: " + suffix);
             }
         }
-
 
         /**
          * This method cannot be called directly.
@@ -237,192 +240,28 @@ public class DrawUtil {
 
 
         /***************************************************************************
-         *  Mouse interactions.
+         *   Geometric drawing method start
          ***************************************************************************/
 
-        /**
-         * Returns true if the mouse is being pressed.
-         *
-         * @return {@code true} if the mouse is being pressed; {@code false} otherwise
-         */
-        public static boolean isMousePressed() {
-            synchronized (mouseLock) {
-                return isMousePressed;
+        private void pixel(double x, double y) {
+            graphics.fillRect((int) Math.round(x), (int) Math.round(y), 1, 1);
+        }
+
+        public void point(double x, double y) {
+            if(penRadius <= 1) pixel(x, y);
+            else{
+                graphics.fill(new Ellipse2D.Double(x - penRadius / 2, y - penRadius / 2, penRadius, penRadius));
             }
-        }
-
-        /**
-         * Returns the <em>x</em>-coordinate of the mouse.
-         *
-         * @return the <em>x</em>-coordinate of the mouse
-         */
-        public static double mouseX() {
-            synchronized (mouseLock) {
-                return mouseX;
-            }
-        }
-
-        /**
-         * Returns the <em>y</em>-coordinate of the mouse.
-         *
-         * @return <em>y</em>-coordinate of the mouse
-         */
-        public static double mouseY() {
-            synchronized (mouseLock) {
-                return mouseY;
-            }
+            show();
         }
 
 
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            // this body is intentionally left empty
-        }
 
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void mouseEntered(MouseEvent e) {
-            // this body is intentionally left empty
-        }
-
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void mouseExited(MouseEvent e) {
-            // this body is intentionally left empty
-        }
-
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void mousePressed(MouseEvent e) {
-            synchronized (mouseLock) {
-                isMousePressed = true;
-            }
-        }
-
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            synchronized (mouseLock) {
-                isMousePressed = false;
-            }
-        }
-
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void mouseDragged(MouseEvent e)  {
-            synchronized (mouseLock) {
-            }
-        }
-
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void mouseMoved(MouseEvent e) {
-            synchronized (mouseLock) {
-            }
-        }
 
 
         /***************************************************************************
-         *  Keyboard interactions.
+         *   Geometric drawing method end
          ***************************************************************************/
-
-        /**
-         * Returns true if the user has typed a key (that has not yet been processed).
-         *
-         * @return {@code true} if the user has typed a key (that has not yet been processed
-         *         by {@link #nextKeyTyped()}; {@code false} otherwise
-         */
-        public static boolean hasNextKeyTyped() {
-            synchronized (keyLock) {
-                return !keysTyped.isEmpty();
-            }
-        }
-
-        /**
-         * Returns the next key that was typed by the user (that your program has not already processed).
-         * This method should be preceded by a call to {@link #hasNextKeyTyped()} to ensure
-         * that there is a next key to process.
-         * This method returns a Unicode character corresponding to the key
-         * typed (such as {@code 'a'} or {@code 'A'}).
-         * It cannot identify action keys (such as F1 and arrow keys)
-         * or modifier keys (such as control).
-         *
-         * @return the next key typed by the user (that your program has not already processed).
-         * @throws NoSuchElementException if there is no remaining key
-         */
-        public static char nextKeyTyped() {
-            synchronized (keyLock) {
-                if (keysTyped.isEmpty()) {
-                    throw new NoSuchElementException("your program has already processed all keystrokes");
-                }
-                return keysTyped.remove(keysTyped.size() - 1);
-                // return keysTyped.removeLast();
-            }
-        }
-
-        /**
-         * Returns true if the given key is being pressed.
-         * <p>
-         * This method takes the keycode (corresponding to a physical key)
-         *  as an argument. It can handle action keys
-         * (such as F1 and arrow keys) and modifier keys (such as shift and control).
-         * See {@link KeyEvent} for a description of key codes.
-         *
-         * @param  keycode the key to check if it is being pressed
-         * @return {@code true} if {@code keycode} is currently being pressed;
-         *         {@code false} otherwise
-         */
-        public static boolean isKeyPressed(int keycode) {
-            synchronized (keyLock) {
-                return keysDown.contains(keycode);
-            }
-        }
-
-
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void keyTyped(KeyEvent e) {
-            synchronized (keyLock) {
-                keysTyped.addFirst(e.getKeyChar());
-            }
-        }
-
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void keyPressed(KeyEvent e) {
-            synchronized (keyLock) {
-                keysDown.add(e.getKeyCode());
-            }
-        }
-
-        /**
-         * This method cannot be called directly.
-         */
-        @Override
-        public void keyReleased(KeyEvent e) {
-            synchronized (keyLock) {
-                keysDown.remove(e.getKeyCode());
-            }
-        }
 
     }
 
