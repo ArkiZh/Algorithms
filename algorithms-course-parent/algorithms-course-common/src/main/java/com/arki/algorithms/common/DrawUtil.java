7b7a08b31b760bdecc90a73dc6750af06a26818f
Rain <rain@example.com>
2018-01-08 22:49:16 +0800

    Doing.

--- algorithms-course-parent/algorithms-course-common/src/main/java/com/arki/algorithms/common/DrawUtil.java
+++ algorithms-course-parent/algorithms-course-common/src/main/java/com/arki/algorithms/common/DrawUtil.java
@@ -10,7 +10,6 @@ import java.awt.image.DirectColorModel;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
 import java.util.List;
 
 public class DrawUtil {
@@ -73,9 +72,16 @@ public class DrawUtil {
 //        }
 //        drawHistogram(list);
         Pen pen = new Pen();
+        pen.setCanvas(500, 500, Color.WHITE);
+        pen.setXrange(0, 100);
+        pen.setYrange(0, 100);
+        pen.setPenRadius(10);
         pen.setPenColor(Color.BLACK);
-        pen.setPenRadius(100);
-        pen.point(200, 200);
+        pen.point(0, 0);
+        pen.setPenRadius(1);
+        pen.fillCircle(50,50,250);
+//        StdDraw.setCanvasSize(500, 500);
+//        StdDraw.rectangle(0, 0, 0.5, 0.5);
 
     }
 
@@ -83,47 +89,44 @@ public class DrawUtil {
     private static final class Pen implements ActionListener {
 
 
-        /**
-         * Components:
-         */
+        /***************************************************************************
+         *  Components.
+         ***************************************************************************/
         private JFrame frame;
         private BufferedImage bufferedImage;
         private Graphics2D graphics;
 
-        /**
-         * Parameters:
-         */
+
+        /***************************************************************************
+         *  Parameters.
+         ***************************************************************************/
+        // Canvas size in pixel.
         private int width = 500;
         private int height = 309;
+        // The region for drawing. It will be mapped to screen coordinate system.
         private double xmin = 0;
         private double xmax = 1;
         private double ymin = 0;
         private double ymax = 1;
+        // Style.
         private Color backgroundColor = Color.WHITE;
         private Color penColor = Color.BLACK;
         private double penRadius = 1;
+        // Coordinate system. WCS, world coordinate system. UCS, user coordinate system.
+        private final double xWorldOrigin = 0;
+        private final double yWorldOrigin = 0;
+        private double xUserOrigin = 0;
+        private double yUserOrigin = 0;
 
 
-
-        // for synchronization
-        private static Object mouseLock = new Object();
-        private static Object keyLock = new Object();
-
-        // mouse state
-        private static boolean isMousePressed = false;
-        private static double mouseX = 0;
-        private static double mouseY = 0;
-
-        // queue of typed key characters
-        private static LinkedList<Character> keysTyped = new LinkedList<Character>();
-
-        // set of key codes currently pressed down
-        private static TreeSet<Integer> keysDown = new TreeSet<Integer>();
-
+        /***************************************************************************
+         *  Init.
+         ***************************************************************************/
         public Pen(){
             init();
         }
         private void init(){
+            if (frame != null) frame.setVisible(false);
             frame = new JFrame();
             bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
             graphics = bufferedImage.createGraphics();
@@ -152,7 +155,7 @@ public class DrawUtil {
         }
 
         /***************************************************************************
-         *  Basic settings.
+         *  Basic settings about drawing style.
          ***************************************************************************/
 
         public void setCanvas(int canvasWidth, int canvasHeight, Color backgroundColor){
@@ -185,6 +188,26 @@ public class DrawUtil {
             menu.add(menuItem);
             return menuBar;
         }
+
+
+        /***************************************************************************
+         *   Coordinate system transfer between user and screen.
+         ***************************************************************************/
+        private double transferWorldToScreenX(double x){ return width * (x + xWorldOrigin - xmin) / (xmax - xmin);}
+        private double transferWorldToScreenY(double y){ return height * (ymax + yWorldOrigin - y) / (ymax - ymin);}
+        private double transferUserToScreenX(double x){ return width * (x + xUserOrigin - xmin) / (xmax - xmin);}
+        private double transferUserToScreenY(double y){ return height * (ymax + yUserOrigin - y) / (ymax - ymin);}
+        private double scaleUserToScreenX(double x){return 0;}// TODO
+
+        public void setXrange(int xmin, int xmax){
+            this.xmin = xmin;
+            this.xmax = xmax;
+        };
+        public void setYrange(int ymin, int ymax){
+            this.ymin = ymin;
+            this.ymax = ymax;
+        };
+
 
         /***************************************************************************
          *  Save drawing to a file.
@@ -239,19 +262,30 @@ public class DrawUtil {
         }
 
 
+
         /***************************************************************************
          *   Geometric drawing method start
          ***************************************************************************/
 
         private void pixel(double x, double y) {
-            graphics.fillRect((int) Math.round(x), (int) Math.round(y), 1, 1);
+            graphics.fillRect((int) Math.round(transferUserToScreenX(x)), (int) Math.round(transferUserToScreenY(y)), 1, 1);
         }
 
         public void point(double x, double y) {
             if(penRadius <= 1) pixel(x, y);
             else{
-                graphics.fill(new Ellipse2D.Double(x - penRadius / 2, y - penRadius / 2, penRadius, penRadius));
+                graphics.fill(new Ellipse2D.Double(transferUserToScreenX(x) - penRadius / 2, transferWorldToScreenY(y) - penRadius / 2, penRadius, penRadius));
             }
+            show();
+        }
+
+        public void circle(double x, double y, double radius) {
+            graphics.draw(new Ellipse2D.Double(transferUserToScreenX(x) - radius, transferUserToScreenY(y) - radius, radius * 2, radius * 2));
+            show();
+        }
+
+        public void fillCircle(double x, double y, double radius) {
+            graphics.fill(new Ellipse2D.Double(transferUserToScreenX(x) - radius, transferUserToScreenY(y) - radius, radius * 2, radius * 2));
             show();
         }
 
